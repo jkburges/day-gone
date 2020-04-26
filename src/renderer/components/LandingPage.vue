5ceac61697ee4e9e78f76fa65fee1f0d252e36f5
Jon Burgess <jkburges@gmail.com>
2020-04-26 17:06:52 +1000

    Store work categories, enable new ones to be added

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -2,41 +2,65 @@
 <div class="container">
   <div>
     <span
-      class="todo-heading">
+      class="work-category-heading">
       What are you doing?
     </span>
   </div>
+
   <div class="work-categories">
     <ul>
       <li
         class="work-category"
-        v-for="workCategory in workCategories "
-        :key="workCategory.id"
+        v-for="workCategory in storeState.workCategories"
         @click="select(workCategory)"
-        >{{workCategory.label}}</li>
+        >{{workCategory}}</li>
     </ul>
+    <form v-on:submit.prevent="noop">
+      <input v-on:keyup="onNewWorkCategory" v-model="newWorkCategory" placeholder="something else">
+    </form>
   </div>
 </div>
 </template>
 
 <script>
+import { store } from "../store.js";
+
 export default {
     data() {
         return {
-            workCategories: [
-                { id: 1, label: "cats" },
-                { id: 2, label: "dogs" },
-                { id: 3, label: "fish" },
-            ]
+            storeState: store.state
         }
     },
+
     methods: {
         select(workCategory) {
-            console.log(`${workCategory.label} selected`);
+            console.log(`${workCategory} selected`);
+        },
+
+        onNewWorkCategory(e) {
+            if (e.keyCode === 13) {
+                store.addWorkCategory(e.target.value);
+            }
+        },
+
+        noop() {
         }
     }
 };
 </script>
 
 <style>
+.container {
+  font-family: system-ui;
+  font-size: 24px;
+  padding: 10px 0px;
+  color: white;
+  height: 100vh;
+  text-align: center;
+  background-color: #30336b;
+}
+
+ul {
+  list-style: none;
+}
 </style>
